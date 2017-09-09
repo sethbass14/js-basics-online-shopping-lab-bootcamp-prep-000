6a019b7c3560b51ec650f7911de16919b8bb279b
Seth Barden <sethbarden@gmail.com>
2017-09-09 15:49:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,19 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+  cart.push({[itemName] : (Math.floor(Math.random() * 100) + 1)})
+  console.log(`${itemName} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
   // write your code here
+  string = "In your cart, you have "
+  for (var i = 1; i < cart.length, i++) {
+    if (i === cart.length - 1) {
+      string = string + ` and `
+    }
 }
 
 function total() {
