f1aab360878cb6a01c7f7e150cc717db76a8ef1d
Seth Barden <sethbarden@gmail.com>
2017-09-09 17:47:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].hasOwnProperty(item)) {
-      cart.splice(i)
+      cart = [...cart.slice(0, i), ...cart.slice(i + 1)]
       return cart
     } else {
       console.log("That item is not in your cart.")
