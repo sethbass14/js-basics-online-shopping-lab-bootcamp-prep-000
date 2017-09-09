35fe83612235d771551d3be144d0506bef7cc11f
Seth Barden <sethbarden@gmail.com>
2017-09-09 17:00:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function viewCart() {
     }
     string = string + `and ${Object.keys(cart[cart.length-1])} at $${cart[cart.length-1][Object.keys(cart[cart.length-1])]}.`
   }
-  console.log(string)
+  console.log("Your shopping cart is empty.")
 }
 
 function total() {
